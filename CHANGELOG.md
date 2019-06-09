c4a8a7400c34981d3a9c3cf8212b6a796c33416e
Wanderson Bragança <wanderson.wbc@gmail.com>
2019-06-08 23:14:00 -0300

    Bug: Fix JavaScrtip events

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,33 +1,33 @@
-yii2-selectivity change Log
-===========================
+# yii2-selectivity change Log
 
-dev-master
-----------
 
-version 2.0.0-beta.1
--------------
+## dev-master
+
+## version 2.0.0-beta.2
+**Date:** 08-Jun-2019
+
+- Bug: Fix JavaScrtip events
+
+## version 2.0.0-beta.1
 **Date:** 06-Jun-2019
 
 - Enh: Selectivity.js dependency updated to ~3.1.0
 
 
-version 1.0.2
--------------
+## version 1.0.2
 **Date:** 07-Nov-2015
 
 - Bug: Ensures correct value for empty attributes
 - Enh: Selectivity.js dependency updated to ^2.0.0
 
 
-version 1.0.1
--------------
+## version 1.0.1
 **Date:** 05-Nov-2015
 
 - Enh: Update .gitnore
 
 
-version 1.0.0
--------------
+## version 1.0.0
 **Date:** 04-Jul-2015
 
 Initial release
--- src/assets/wb-widget-selectivity.js
+++ src/assets/wb-widget-selectivity.js
@@ -1,18 +1,29 @@
-var WbWidgetSelectivity = function(target) {
+var WbWidgetSelectivity = function (target) {
   this.originalInput = document.querySelector('#' + target);
-  this.selectivityInput = document.querySelector('.field-' + target +' .selectivity-input');
+  this.selectivityInput = document.querySelector('.field-' + target + ' .selectivity-input');
 
-  this.setObserver = function() {
+  this.setObserver = function () {
     var $self = this;
-    var mutationObserver = new MutationObserver(function(mutations) {
+    var mutationObserver = new MutationObserver(function (mutations) {
       var className = mutations[0].target.className;
       $self.selectivityInput.setAttribute("class", className.trim() + " selectivity-input");
+
+      var originalInput = jQuery('#' + target);
+      var data = originalInput.selectivity('data');
+
+      if (Array.isArray(data)) {
+        var values = data.map(function (item) { return item.id; });
+        originalInput.val(values);
+      } else if (data && typeof data === "object") {
+        originalInput.val(data['id']);
+      }
+
+      $self.originalInput.dispatchEvent(new Event('change'));
     });
 
     mutationObserver.observe(this.originalInput, {
       attributes: true,
       attributeOldValue: true,
-      attributeFilter: ['class'],
       subtree: true
     });
   }
